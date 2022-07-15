638226aff8ff704943418791bedf6585b0bcfc99
WaiYanML <107084949+WaiYanML@users.noreply.github.com>
2022-07-15 21:31:56 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+First, the data is saved in the working directory.
+Then, it is loaded and train and test data are combined.
+Variable names with mean and std are extracted.
+Activity names are set to descriptive names.
+Variable names are also set.
+Tidy data for each subject and each activity is formed.
+Additional information is included in the code file as comment.
---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+load_data<- function(){
+  #loading packages
+  library(tidyr)
+  library(dplyr)
+  
+  ##loading train and test data
+  xtrain<-read.table("./UCI HAR Dataset/train/X_train.txt")
+  ytrain<-read.table("./UCI HAR Dataset/train/y_train.txt")
+  subtrain<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+  
+  xtest<-read.table("./UCI HAR Dataset/test/X_test.txt")
+  ytest<-read.table("./UCI HAR Dataset/test/y_test.txt")
+  subtest<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+  
+  #loading feature and activity names
+  features<-read.table("./UCI HAR Dataset/features.txt")
+  activity<-read.table("./UCI HAR Dataset/activity_labels.txt")
+  
+  ##combining train and test data for x,y and subjects
+  x<-rbind(xtrain,xtest)
+  y<-rbind(ytrain,ytest)
+  subj<-rbind(subtrain,subtest)
+  
+  ##substituting activity names
+  activity<- activity[y[[1]],2]
+  
+  ##naming variables
+  colnames(x)<- features[[2]]
+  colnames(subj)<- "subject"
+  
+  ##filtering out mean and std data
+  x<-select(x,contains("mean()"),contains("std()"))
+  
+  ##combining to a single dataset
+  proj_data<-cbind(subj,activity,x)
+  
+  grouped_data<-group_by(proj_data,subject, activity)
+  summ_data<-summarise(grouped_data,value=colMeans(grouped_data[,3:68]),ind=1:66)
+  summ_data$ind<-colnames(proj_data)[3:68][summ_data$ind]
+  tidy_data<-spread(summ_data,ind, value)
+  tidy_data<-select(tidy_data,subject,everything())
+
+  tidy_data 
+  
+}
